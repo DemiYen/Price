b35c45357056ab01a669a3c55e25f8219d0a4d5d
DemiYen <demimi20@gmail.com>
2021-06-02 03:53:59 +0800

    Update Price.java

--- app/src/main/java/price/Price.java
+++ app/src/main/java/price/Price.java
@@ -1,95 +1,51 @@
-
-package price;
-
-public class Price {
-
-	public boolean coach;
-	public int timeIn,timeOut,people,coachQ,studentQ;
-	public String timeInterval;
-
-	public Price(boolean coach, int timeIn, int timeOut, 
-					String timeInterval, int people, int coachQ, int studentQ){
-		this.coach = coach;
-		this.timeIn = timeIn;
-		this.timeOut = timeOut;
-		this.timeInterval = timeInterval;
-		this.people = people;
-		this.coachQ = coachQ;
-		this.studentQ = studentQ;
-	}
-
-	public int goSwim() {
-		int PRICE = 0;
-		
-		if(coach == false) {
-			PRICE = 200 * people;
-		}else{
-			if(people < 10) {
-				PRICE = (timeOut - timeIn) * 3000;
-			}else{
-				if(timeInterval.equals("A")) {
-					PRICE = (120 * coachQ) + (70 * studentQ);
-				}else if(timeInterval.equals("AB")) {
-					PRICE = ((120 * coachQ) + (70 * studentQ)) * 2;
-				}else if(timeInterval.equals("ABC")) {
-					PRICE = ((120 * coachQ) + (70 * studentQ)) * 3;
-				}else if(timeInterval.equals("ABCD")) {
-					PRICE = ((120 * coachQ) + (70 * studentQ)) * 4;
-				}else {
-					PRICE = 0;
-				}
-			}
-		}	
-		return PRICE;
+public class PriceTest {
+	@Test
+	public void testGoSwim1() {
+		Price p = new Price(false, 8, 9, null, 10, 0, 10);
+		assertEquals(2000, p.goSwim());
 	}
 	
-}
-		
-		
-package price;
-
-public class Price {
-
-	public boolean coach;
-	public int timeIn,timeOut,people,coachQ,studentQ;
-	public String timeInterval;
-
-	public Price(boolean coach, int timeIn, int timeOut, 
-					String timeInterval, int people, int coachQ, int studentQ){
-		this.coach = coach;
-		this.timeIn = timeIn;
-		this.timeOut = timeOut;
-		this.timeInterval = timeInterval;
-		this.people = people;
-		this.coachQ = coachQ;
-		this.studentQ = studentQ;
-	}
-
-	public int goSwim() {
-		int PRICE = 0;
-		
-		if(coach == false) {
-			PRICE = 200 * people;
-		}else{
-			if(people < 10) {
-				PRICE = (timeOut - timeIn) * 3000;
-			}else{
-				if(timeInterval.equals("A")) {
-					PRICE = (120 * coachQ) + (70 * studentQ);
-				}else if(timeInterval.equals("AB")) {
-					PRICE = ((120 * coachQ) + (70 * studentQ)) * 2;
-				}else if(timeInterval.equals("ABC")) {
-					PRICE = ((120 * coachQ) + (70 * studentQ)) * 3;
-				}else if(timeInterval.equals("ABCD")) {
-					PRICE = ((120 * coachQ) + (70 * studentQ)) * 4;
-				}else {
-					PRICE = 0;
-				}
-			}
-		}	
-		return PRICE;
+	@Test
+	public void testGoSwim2() {
+		Price p = new Price(true, 8, 9, null, 7, 0, 7);
+		assertEquals(3000, p.goSwim());
 	}
 	
+	@Test
+	public void testGoSwim3() {
+		Price p = new Price(true, 16, 18, null, 5, 0, 5);
+		assertEquals(6000, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim4() {
+		Price p = new Price(true, 0, 0, "A", 20, 5, 15);
+		assertEquals(1650, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim5() {
+		Price p = new Price(true, 0, 0, "AB", 15, 3, 12);
+		assertEquals(2400, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim6() {
+		Price p = new Price(true, 0, 0, "ABC", 13, 2, 11);
+		assertEquals(3030, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim7() {
+		Price p = new Price(true, 0, 0, "ABCD", 25, 6, 19);
+		assertEquals(8200, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim8() {
+		Price p = new Price(true, 0, 0, "", 25, 6, 19);
+		assertEquals(0, p.goSwim());
+	}
+	
+
 }
-		
-		
