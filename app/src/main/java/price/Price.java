acf7e87ad157388d80dd82e891787ebbaba40ab8
DemiYen <demimi20@gmail.com>
2021-06-02 03:52:58 +0800

    test

--- app/src/main/java/price/Price.java
+++ app/src/main/java/price/Price.java
@@ -23,28 +23,67 @@ public class Price {
 		
 		if(coach == false) {
 			PRICE = 200 * people;
-		}else if(coach == true) {
+		}else{
 			if(people < 10) {
 				PRICE = (timeOut - timeIn) * 3000;
-			}else if(people >= 10) {
-				if(timeInterval.equals("A") || timeInterval.equals("B") || timeInterval.equals("C")
-						|| timeInterval.equals("D") || timeInterval.equals("E")) {
+			}else{
+				if(timeInterval.equals("A")) {
 					PRICE = (120 * coachQ) + (70 * studentQ);
-				}else if(timeInterval.equals("AB") || timeInterval.equals("AC") || timeInterval.equals("AD") 
-						|| timeInterval.equals("AE") || timeInterval.equals("BC") || timeInterval.equals("BD")
-						|| timeInterval.equals("BE") || timeInterval.equals("CD") || timeInterval.equals("CE")
-						|| timeInterval.equals("DE")) {
+				}else if(timeInterval.equals("AB")) {
 					PRICE = ((120 * coachQ) + (70 * studentQ)) * 2;
-				}else if(timeInterval.equals("ABC") || timeInterval.equals("ABD") || timeInterval.equals("ABE")
-						|| timeInterval.equals("ACD") || timeInterval.equals("ACE") || timeInterval.equals("ADE")
-						|| timeInterval.equals("BCD") || timeInterval.equals("BCE") || timeInterval.equals("BDE")
-						|| timeInterval.equals("CDE")) {
+				}else if(timeInterval.equals("ABC")) {
 					PRICE = ((120 * coachQ) + (70 * studentQ)) * 3;
-				}else if(timeInterval.equals("ABCD") || timeInterval.equals("ABCE") || timeInterval.equals("ABDE")
-						|| timeInterval.equals("ACDE") || timeInterval.equals("BCDE")) {
+				}else if(timeInterval.equals("ABCD")) {
 					PRICE = ((120 * coachQ) + (70 * studentQ)) * 4;
-				}else if(timeInterval.equals("ABCDE")) {
-					PRICE = ((120 * coachQ) + (70 * studentQ)) * 5;
+				}else {
+					PRICE = 0;
+				}
+			}
+		}	
+		return PRICE;
+	}
+	
+}
+		
+		
+package price;
+
+public class Price {
+
+	public boolean coach;
+	public int timeIn,timeOut,people,coachQ,studentQ;
+	public String timeInterval;
+
+	public Price(boolean coach, int timeIn, int timeOut, 
+					String timeInterval, int people, int coachQ, int studentQ){
+		this.coach = coach;
+		this.timeIn = timeIn;
+		this.timeOut = timeOut;
+		this.timeInterval = timeInterval;
+		this.people = people;
+		this.coachQ = coachQ;
+		this.studentQ = studentQ;
+	}
+
+	public int goSwim() {
+		int PRICE = 0;
+		
+		if(coach == false) {
+			PRICE = 200 * people;
+		}else{
+			if(people < 10) {
+				PRICE = (timeOut - timeIn) * 3000;
+			}else{
+				if(timeInterval.equals("A")) {
+					PRICE = (120 * coachQ) + (70 * studentQ);
+				}else if(timeInterval.equals("AB")) {
+					PRICE = ((120 * coachQ) + (70 * studentQ)) * 2;
+				}else if(timeInterval.equals("ABC")) {
+					PRICE = ((120 * coachQ) + (70 * studentQ)) * 3;
+				}else if(timeInterval.equals("ABCD")) {
+					PRICE = ((120 * coachQ) + (70 * studentQ)) * 4;
+				}else {
+					PRICE = 0;
 				}
 			}
 		}	
--- app/src/test/java/price/PriceTest.java
+++ app/src/test/java/price/PriceTest.java
@@ -9,5 +9,47 @@ public class PriceTest {
 		assertEquals(2000, p.goSwim());
 	}
 	
+	@Test
+	public void testGoSwim2() {
+		Price p = new Price(true, 8, 9, null, 7, 0, 7);
+		assertEquals(3000, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim3() {
+		Price p = new Price(true, 16, 18, null, 5, 0, 5);
+		assertEquals(6000, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim4() {
+		Price p = new Price(true, 0, 0, "A", 20, 5, 15);
+		assertEquals(1650, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim5() {
+		Price p = new Price(true, 0, 0, "AB", 15, 3, 12);
+		assertEquals(2400, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim6() {
+		Price p = new Price(true, 0, 0, "ABC", 13, 2, 11);
+		assertEquals(3030, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim7() {
+		Price p = new Price(true, 0, 0, "ABCD", 25, 6, 19);
+		assertEquals(8200, p.goSwim());
+	}
+	
+	@Test
+	public void testGoSwim8() {
+		Price p = new Price(true, 0, 0, "", 25, 6, 19);
+		assertEquals(0, p.goSwim());
+	}
+	
 
 }
